6b2302aff04e521a1b762ad4803ab07ed18fd786
Mussie Tarekegn Baye <mussie.tarekegn@aastustudent.edu.et>
2025-12-14 21:30:44 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,18 +10,15 @@ python manage.py collectstatic --noinput
 # 3. Apply migrations
 python manage.py migrate
 
-# 4. Create superuser automatically if it doesn't exist
-python manage.py shell << EOF
+# 4. Create superuser if it doesn't exist (force staff and superuser)
+python manage.py shell -c "
 from django.contrib.auth import get_user_model
 User = get_user_model()
 if not User.objects.filter(username='hp').exists():
-    User.objects.create_superuser('hp', 'moss@gmail.com', 'Moss123@')
-EOF
-
-# 5. Print all users and their staff/superuser status (for verification)
-python manage.py shell << EOF
-from django.contrib.auth import get_user_model
-User = get_user_model()
-for u in User.objects.all():
-    print(f"Username: {u.username}, is_staff: {u.is_staff}, is_superuser: {u.is_superuser}")
-EOF
+    User.objects.create_superuser(username='hp', email='moss@gmail.com', password='Moss123@')
+u = User.objects.get(username='hp')
+u.is_staff = True
+u.is_superuser = True
+u.save()
+print(f'Created/Updated superuser: {u.username}, is_staff={u.is_staff}, is_superuser={u.is_superuser}')
+"
